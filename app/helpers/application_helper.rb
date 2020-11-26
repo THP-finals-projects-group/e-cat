dabcc463f39d2eed640cec285c8534c76f71d5f2
Erudigit <duguet.vincent@outlook.fr>
2020-11-26 16:04:09 +0100

    disallow order if cart.total empty

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+    def is_cart_empty(cart_total)
+        if cart_total > 0
+            return false
+        else 
+            return true
+        end
+    end
 end
