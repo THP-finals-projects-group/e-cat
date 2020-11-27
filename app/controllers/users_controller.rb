58f8d504db4130bd7530463b3be5dcfa82bd181a
qdtrl <quentindtouroul@gmail.com>
2020-11-27 04:39:20 +0100

    admin order + show user orders

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,5 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         @orders = Order.where(user_id: @user.id).includes(:order_items, :products)
-
-        @products_title = []
-        @products_price = []
-        @products_quantity = []
-        @orders.each do |order|
-            @orderitems = OrderItem.where(order_id: order.id)
-            @orderitems.each do |orderitem|
-                @products_title << Product.find(orderitem.product_id).title
-                @products_quantity << orderitem.quantity
-                @products_price << Product.find(orderitem.product_id).price
-            end
-        end
     end
 end
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -16,11 +16,34 @@ class OrderMailer < ApplicationMailer
 
     @url = "https://dev-e-cat.herokuapp.com/"   
 
-    #attachments[@product.image.name + ".png"] = to_png(@product.image) 
     mail(
       from: "dev-a-cat@yopmail.com", 
       to: @user.email,
       subject: "Félicitation pour votre a-chat de chat !",
+      delivery_method_options: { api_key: ENV['MAILJET_API'], secret_key: ENV['MAILJET_SECRET_KEY'] }
+    )
+    
+  end
+  def order_mail_admin(order)
+    @user = User.find(order.user_id)
+    @order = Order.find(order.id)
+    @products_title = []
+    @products_description = []
+    @products_price = []
+    @orderitems = OrderItem.where(order_id: order.id)
+    @orderitems.each do |orderitem|
+      @products_title << Product.find(orderitem.product_id).title
+      @products_description << Product.find(orderitem.product_id).description
+      @products_price << Product.find(orderitem.product_id).price
+    end
+
+
+    @url = "https://dev-e-cat.herokuapp.com/"   
+
+    mail(
+      from: "dev-a-cat@yopmail.com", 
+      to: "admin-e-cat@yopmail.com",
+      subject: "A-chat de chat de la part de #{@user.email} !",
       delivery_method_options: { api_key: ENV['MAILJET_API'], secret_key: ENV['MAILJET_SECRET_KEY'] }
     )
     
